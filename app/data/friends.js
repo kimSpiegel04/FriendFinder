a66f43d0758acf2d412767c07c24249df08bee2b
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-08-02 12:19:24 -0400

    set up data array and connected

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friendsArray = [
+    {
+        "name":"Merlin",
+        "photo":"https://static.boredpanda.com/blog/wp-content/uploads/2017/12/angry-cat-merlin-ragdoll-13-5a2f9000502f0__700.jpg",
+        "scores":[
+            1,
+            5,
+            5,
+            1,
+            1,
+            5,
+            5,
+            5,
+            1,
+            1
+        ]
+    },
+    {
+        "name":"Huxley",
+        "photo":"https://i.pinimg.com/236x/4b/26/cc/4b26cc4097c6e35391772343d0285d15.jpg",
+        "scores":[
+            5,
+            1,
+            1,
+            5,
+            5,
+            1,
+            1,
+            1,
+            5,
+            5
+        ]
+    }
+];
+
+module.exports = friendsArray;
+
